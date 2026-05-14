f9f04494244a74dc0486d48983cd0570fe8366cd
Yatish Kumar <yatish0155@gmail.com>
2026-05-14 12:35:20 +0530

    refactor: move firebase configuration to environment variables in count_prompts script

--- scripts/migrations/count_prompts.js
+++ scripts/migrations/count_prompts.js
@@ -1,13 +1,14 @@
+require('dotenv').config({ path: '.env.local' });
 const { initializeApp } = require('firebase/app');
 const { getFirestore, collection, getDocs } = require('firebase/firestore');
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDAtvqaprh18Eb6QSGr2nxRtriP-v8LRco",
-  authDomain: "promptapp-12345.firebaseapp.com",
-  projectId: "promptapp-12345",
-  storageBucket: "promptapp-12345.firebasestorage.app",
-  messagingSenderId: "375289943916",
-  appId: "1:375289943916:web:db9b1ee77edbfd5c5f4549"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
